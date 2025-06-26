87c34fcd320d4675a90c3c8dbb05cab732aec570
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-06-26 21:09:52 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,7 @@
+from threading import Thread
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+def keep_alive():
+    server = HTTPServer(("0.0.0.0", 8080), SimpleHTTPRequestHandler)
+    Thread(target=server.serve_forever).start()
 
