bb75fbedd220314d5e581e67cd5be39baf0ef8c5
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-06-28 01:10:14 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,14 +2,15 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Lumi bot is up and running!"
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    Thread(target=app.run, kwargs={
-        'host': '0.0.0.0',
-        'port': 8080
-    }).start()
+    t = Thread(target=run)
+    t.start()
