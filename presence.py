d9ca1edfebcdf19b85f94a136c5b2e18a44df03d
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-06-27 22:10:41 +0530

    Update presence.py

--- presence.py
+++ presence.py
@@ -6,6 +6,6 @@ import discord
 async def set_rich_presence(client: discord.Client):
     activity = discord.Activity(
         type=discord.ActivityType.watching,
-        name="💖 you sleep"
+        name="💖 you ping me"
     )
     await client.change_presence(status=discord.Status.idle, activity=activity)
