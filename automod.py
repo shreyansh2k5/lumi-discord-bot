1eaac7fad46d2655726026c7b79dc461d53ab48f
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-06-29 16:37:19 +0530

    Update automod.py

--- automod.py
+++ automod.py
@@ -1,11 +1,15 @@
 # automod.py
 
-# List of bad words (lowercase). You can expand this list.
+# List of banned words (you can expand this)
 banned_words = {
-    "idiot", "stupid", "dumb", "shit", "fuck", "bitch", "asshole", "kill", "moron"
+    "idiot", "stupid", "dumb", "shit", "fuck", "bitch", "asshole", "kill", "moron",
+    "retard", "slut", "bastard", "crap", "faggot"
 }
 
 def check_bad_words(message: str) -> bool:
-    """Returns True if the message contains any banned words."""
+    """
+    Checks if the given message contains any banned words.
+    Returns True if any are found, else False.
+    """
     content = message.lower()
     return any(bad_word in content for bad_word in banned_words)
