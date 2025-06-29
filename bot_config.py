a8cfed906ef802cd05c7d9dd6658d84fdc53f22a
shreyansh2k5 <119129251+shreyansh2k5@users.noreply.github.com>
2025-06-29 16:45:52 +0530

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -5,7 +5,9 @@ from discord.ext import commands
 from groq_api import query_groq as query_model
 from personality import apply_personality
 from memory_store import get_memory, add_to_memory
-from automod import check_bad_words, has_restricted_role
+from automod import check_bad_words
+from role_exceptions import is_role_exempt
+
 
 intents = discord.Intents.default()
 intents.message_content = True
